f486f108954d0a188c84977b7f01ef99fd672b20
Mark Woo <wcgwuxinwei@gmail.com>
2019-03-01 20:57:25 +0800

    fix issue about failed to build

--- dingtalk_test.go
+++ dingtalk_test.go
@@ -48,7 +48,7 @@ func TestDingTalk_SendMessage(t *testing.T) {
 			MsgType:     MsgTypeMarkdown,
 			AccessToken: "3946b02aae68e6a03138f60740645bb691bc802f1fa307acba9d2eddc1b516ae",
 			Markdown: &Markdown{
-			    //Title: `test alert`,
+			    Title: `test alert`,
 			    Text: "#测试告警邮件\n在**2019-03-01T20:18:52+08:00**到**2019-03-01T20:18:52+08:00**期间\n发生了告警",
 				At: At{
 					IsAtAll: true,
